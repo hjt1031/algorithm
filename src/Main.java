6b3be577fa6de3af0c7bb42967cc7f0b3aac4dbc
hjt1031 <hjt1031@naver.com>
2025-01-18 21:47:15 +0900

    프로그래머스 PCCE_8 지폐접기

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import algorithmTest.Pcce_9;
+
+public class Main {
+    public static void main(String[] args) {
+//        Pcce_9 pcce9 = new Pcce_9();
+//        System.out.println("solutions.solution Result: " + pcce9.pcce_9(new int[]{50, 50}, new int[]{100, 241}));
+    }
+}
---
+++ src/algorithmTest/Pcce_9.java
@@ -0,0 +1,24 @@
+package algorithmTest;
+
+public class Pcce_9 {
+    public int pcce_9(int[] wallet, int[] bill) {
+        int answer = 0;
+        while(true) {
+            int walletMin = Math.min(wallet[0], wallet[1]);
+            int walletMax = Math.max(wallet[0], wallet[1]);
+            int billMin = Math.min(bill[0], bill[1]);
+            int billMax = Math.max(bill[0], bill[1]);
+            if ((billMin > walletMin) || (billMax > walletMax)) {
+                if (bill[0] > bill[1]) {
+                    bill[0] = bill[0] / 2;
+                } else {
+                    bill[1] = bill[1] / 2;
+                }
+                answer++;
+            } else {
+                break;
+            }
+        }
+        return answer;
+    }
+}
